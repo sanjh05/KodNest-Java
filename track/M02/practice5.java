f792a3e423f57bb4af349637e16e8f49c1fef7ec
sanjh <patelsanjh05@gmail.com>
2026-08-04 12:26:47 +0530

    Practice5-1

--- track/M02/practice5.java
+++ track/M02/practice5.java
@@ -1,6 +1,8 @@
 public class practice5 {
     public static void main(String[] args) {
-        int num = 0;
+        int num = 10;
+        // int num = -1 it will print negative
+        // int num = 0 it will print zero
         if (num > 0) {
             System.out.println("Positive");
         } else if (num < 0) {
