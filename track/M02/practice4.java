adba5a8bed70441bfdd6c8cbe57e3c4fb5120db6
sanjh <patelsanjh05@gmail.com>
2026-08-04 12:25:34 +0530

    Practice5

--- track/M02/practice4.java
+++ track/M02/practice4.java
@@ -1,11 +1,11 @@
-public class practice4{
-    public static void main(String[] args){
-         System.out.println("Monday");
-         System.out.print("Tuesday");
-          System.out.println("Wednesday");
-           System.out.println("Thursday");
-            System.out.print("Friday");
-             System.out.println("Saturday");
-              System.out.println("Sunday");
+public class practice4 {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
     }
 }
---
+++ track/M02/practice5.java
@@ -0,0 +1,12 @@
+public class practice5 {
+    public static void main(String[] args) {
+        int num = 0;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
