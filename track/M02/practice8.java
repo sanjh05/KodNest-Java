d97f6f7f4cddc2c092d7b000e18be39430e0673a
sanjh <patelsanjh05@gmail.com>
2026-08-05 11:15:34 +0530

    Practice8

---
+++ track/M02/practice8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class practice8{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        
+        switch(num){
+            case 1:  System.out.println("Monday");
+            break;
+            case 2:  System.out.println("Tuesday");
+            break;
+            case 3:  System.out.println("Wednesday");
+            break;
+            case 4:  System.out.println("Thursday");
+            break;
+            default: System.out.println("Invalid");
+        
+        }
+
+    }
+}
