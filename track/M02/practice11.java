ce8432491a18cd05641f2243a5fe81535fab4d4c
sanjh <patelsanjh05@gmail.com>
2026-08-06 13:03:44 +0530

    Practice12

--- track/M02/practice11.java
+++ track/M02/practice11.java
@@ -1,7 +1,7 @@
 public class practice11 {
     public static void main(String[] args) {
 
-
+        //break
         for(int i = 1; i <= 3; i++){
             for(int j = 1; j <= 2; j++){
                 if(j == 2){
@@ -14,7 +14,7 @@ public class practice11 {
 
          System.out.println();
 
-
+         //continue
         for(int i = 1; i <= 3; i++){
             for(int j = 1; j <= 5; j++){
                 if(j == 2){
